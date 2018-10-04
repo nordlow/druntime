e9288ee87cb02b435eb1de9df64efa5dcfcb2eb6
Per Nordlöw <per.nordlow@gmail.com>
2018-10-04 22:15:43 +0200

    *** empty log message ***

--- src/rt/util/container/static_bitarray.d
+++ src/rt/util/container/static_bitarray.d
@@ -54,11 +54,47 @@ struct StaticBitArray(uint bitCount, Block = size_t)
         return b;
     }
 
-    // TODO find index of first zero bit
-    size_t indexOfFirst1() const nothrow
+    /** Get number of bits set. */
+    size_t countOnes() const
+    {
+        typeof(return) n = 0;
+        foreach (const block; _blocks)
+        {
+            import core.bitop : popcnt;
+            static if (block.sizeof == 1 ||
+                       block.sizeof == 2 ||
+                       block.sizeof == 4 ||
+                       block.sizeof == 4)
+            {
+                // TODO do we need to force `uint`-overload of `popcnt`?
+                n += cast(uint)block.popcnt;
+            }
+            else static if (block.sizeof == 8)
+            {
+                n += (cast(ulong)((cast(uint)(block)).popcnt) +
+                      cast(ulong)((cast(uint)(block >> 32)).popcnt));
+            }
+            else
+            {
+                assert(0, "Unsupported Block size " ~ Block.sizeof.stringof);
+            }
+        }
+        return typeof(return)(n);
+    }
+
+    /** Find index of first non-zero bit or `length` if no bit set. */
+    size_t indexOfFirstSetBit() const nothrow
     {
         pragma(inline, true);
-        return 0;               // TODO
+        import core.bitop : bsf;
+        foreach (const blockIndex, const block; _blocks)
+        {
+            if (block != 0)
+            {
+                return blockIndex*bitsPerBlock + bsf(block);
+            }
+        }
+        return length;
     }
 
     private Block[blockCount] _blocks;
@@ -69,9 +105,27 @@ struct StaticBitArray(uint bitCount, Block = size_t)
 ///
 unittest
 {
-    StaticBitArray!63 x;
+    alias Block = size_t;
+    enum blockCount = 2;
+    enum length = blockCount * 8*Block.sizeof - 1;
+    StaticBitArray!(length) x;
+    static assert(x.blockCount == blockCount);
+
+    assert((x.indexOfFirstSetBit == x.length));
+    x[length - 1] = true;
+    assert((x.indexOfFirstSetBit == x.length - 1));
+    x[length - 2] = true;
+    assert((x.indexOfFirstSetBit == x.length - 2));
+
+    x[length/2 + 1] = true;
+    assert((x.indexOfFirstSetBit == x.length/2 + 1));
+    x[length/2] = true;
+    assert((x.indexOfFirstSetBit == x.length/2));
+    x[length/2 - 1] = true;
+    assert((x.indexOfFirstSetBit == x.length/2 - 1));
 
     x[0] = true;
+    assert((x.indexOfFirstSetBit == 0));
     assert(x[0]);
     assert(!x[1]);
 
