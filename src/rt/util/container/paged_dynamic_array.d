5696bd73944565b8c86c370f87433af037a7e0b6
Per Nordlöw <per.nordlow@gmail.com>
2018-10-05 10:13:12 +0200

    *** empty log message ***

---
+++ src/rt/util/container/paged_dynamic_array.d
@@ -0,0 +1,8 @@
+/**
+ * Array container with paged allocation for internal usage.
+ *
+ * Copyright: Copyright Per Nordlöw 2018.
+ * License:   $(HTTP www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+ * Authors:   Per Nordlöw
+ */
+module rt.util.container.paged_dynamic_array;
