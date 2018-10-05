8abff7d8b6e112fb9e848c86e7c901f9b018a63d
Per Nordlöw <per.nordlow@gmail.com>
2018-10-05 10:51:03 +0200

    *** empty log message ***

--- src/rt/util/container/paged_dynamic_array.d
+++ src/rt/util/container/paged_dynamic_array.d
@@ -6,3 +6,272 @@
  * Authors:   Per Nordlöw
  */
 module rt.util.container.paged_dynamic_array;
+
+static import common = rt.util.container.common;
+
+struct PageDynamicArray(T)
+{
+    import gc.os : os_mem_map, os_mem_unmap;
+    import core.exception : onOutOfMemoryErrorNoGC;
+
+    nothrow:
+
+    @disable this(this);
+
+    enum PAGESIZE = 4096;       // Linux $(shell getconf PAGESIZE)
+
+    ~this()
+    {
+        reset();
+    }
+
+    void reset()
+    {
+        length = 0;
+    }
+
+    @property size_t length() const
+    {
+        return _length;
+    }
+
+    @property size_t capacityInBytes() const
+    {
+        pragma(inline, true);
+        return _capacityInPages*PAGESIZE;
+    }
+
+    @property void length(size_t newLength)
+    {
+        import core.checkedint : mulu;
+
+        if (newLength*T.sizeof > capacityInBytes)
+        {
+            bool overflow = false;
+            const size_t reqsize = mulu(T.sizeof, newLength, overflow);
+            const size_t newCapacityInPages = reqsize/PAGESIZE + (reqsize%PAGESIZE ? 1 : 0);
+            if (overflow)
+            {
+                onOutOfMemoryErrorNoGC();
+            }
+
+            import core.internal.traits : hasElaborateDestructor;
+            static if (hasElaborateDestructor!T)
+            {
+                static assert("destroy T");
+                //
+                // if (newLength < _length)
+                // {
+                //     foreach (ref val; _ptr[newLength .. _length])
+                //     {
+                //         common.destroy(val);
+                //     }
+                // }
+            }
+
+            if (newLength != 0)
+            {
+                auto oldptr = _ptr;
+                _ptr = cast(T*)os_mem_map(newCapacityInPages*PAGESIZE);
+                import core.stdc.string : memcpy;
+                memcpy(ptr, oldptr, capacityInBytes);
+            }
+            os_mem_unmap(_ptr, capacityInBytes);
+
+            // rely on mmap zeroing for us
+            // if (newLength > _length)
+            // {
+            //     foreach (ref val; _ptr[_length .. newLength])
+            //     {
+            //         common.initialize(val);
+            //     }
+            // }
+        }
+
+        _length = newLength;
+    }
+
+    @property bool empty() const
+    {
+        return !length;
+    }
+
+    @property ref inout(T) front() inout
+    in { assert(!empty); }
+    do
+    {
+        return _ptr[0];
+    }
+
+    @property ref inout(T) back() inout
+    in { assert(!empty); }
+    do
+    {
+        return _ptr[_length - 1];
+    }
+
+    ref inout(T) opIndex(size_t idx) inout
+    in { assert(idx < length); }
+    do
+    {
+        return _ptr[idx];
+    }
+
+    inout(T)[] opSlice() inout
+    {
+        return _ptr[0 .. _length];
+    }
+
+    inout(T)[] opSlice(size_t a, size_t b) inout
+    in { assert(a < b && b <= length); }
+    do
+    {
+        return _ptr[a .. b];
+    }
+
+    inout(T)* ptr() inout @system
+    {
+        return _ptr;
+    }
+
+    alias length opDollar;
+
+    void insertBack()(auto ref T val)
+    {
+        import core.checkedint : addu;
+        bool overflow = false;
+        const size_t newlength = addu(length, 1, overflow);
+        if (overflow)
+        {
+            onOutOfMemoryErrorNoGC();
+        }
+        length = newlength;
+        back = val;
+    }
+
+    void popBack()
+    {
+        // TODO destroy back element if needed
+        length = length - 1;
+    }
+
+    void remove(size_t idx)
+    in { assert(idx < length); }
+    do
+    {
+        foreach (i; idx .. length - 1)
+            _ptr[i] = _ptr[i+1];
+        popBack();
+    }
+
+    void swap(ref PageDynamicArray other)
+    {
+        auto ptr = _ptr;
+        _ptr = other._ptr;
+        other._ptr = ptr;
+        immutable len = _length;
+        _length = other._length;
+        other._length = len;
+    }
+
+    invariant
+    {
+        assert(!_ptr == !_length);
+    }
+
+private:
+    T* _ptr;
+    size_t _length;
+    size_t _capacityInPages;    // of size `PAGESIZE`
+}
+
+unittest
+{
+    PageDynamicArray!size_t ary;
+
+    assert(ary[] == []);
+    ary.insertBack(5);
+    assert(ary[] == [5]);
+    assert(ary[$-1] == 5);
+    ary.popBack();
+    assert(ary[] == []);
+    ary.insertBack(0);
+    ary.insertBack(1);
+    assert(ary[] == [0, 1]);
+    assert(ary[0 .. 1] == [0]);
+    assert(ary[1 .. 2] == [1]);
+    assert(ary[$ - 2 .. $] == [0, 1]);
+    size_t idx;
+    foreach (val; ary) assert(idx++ == val);
+    foreach_reverse (val; ary) assert(--idx == val);
+    foreach (i, val; ary) assert(i == val);
+    foreach_reverse (i, val; ary) assert(i == val);
+
+    ary.insertBack(2);
+    ary.remove(1);
+    assert(ary[] == [0, 2]);
+
+    assert(!ary.empty);
+    ary.reset();
+    assert(ary.empty);
+    ary.insertBack(0);
+    assert(!ary.empty);
+    destroy(ary);
+    assert(ary.empty);
+
+    // not copyable
+    static assert(!__traits(compiles, { PageDynamicArray!size_t ary2 = ary; }));
+    PageDynamicArray!size_t ary2;
+    static assert(!__traits(compiles, ary = ary2));
+    static void foo(PageDynamicArray!size_t copy) {}
+    static assert(!__traits(compiles, foo(ary)));
+
+    ary2.insertBack(0);
+    assert(ary.empty);
+    assert(ary2[] == [0]);
+    ary.swap(ary2);
+    assert(ary[] == [0]);
+    assert(ary2.empty);
+}
+
+unittest
+{
+    alias RC = common.RC;
+    PageDynamicArray!RC ary;
+
+    size_t cnt;
+    assert(cnt == 0);
+    ary.insertBack(RC(&cnt));
+    assert(cnt == 1);
+    ary.insertBack(RC(&cnt));
+    assert(cnt == 2);
+    ary.back = ary.front;
+    assert(cnt == 2);
+    ary.popBack();
+    assert(cnt == 1);
+    ary.popBack();
+    assert(cnt == 0);
+}
+
+unittest
+{
+    import core.exception;
+    try
+    {
+        // Overflow ary.length.
+        auto ary = PageDynamicArray!size_t(cast(size_t*)0xdeadbeef, -1);
+        ary.insertBack(0);
+    }
+    catch (OutOfMemoryError)
+    {
+    }
+    try
+    {
+        // Overflow requested memory size for common.xrealloc().
+        auto ary = PageDynamicArray!size_t(cast(size_t*)0xdeadbeef, -2);
+        ary.insertBack(0);
+    }
+    catch (OutOfMemoryError)
+    {
+    }
+}
