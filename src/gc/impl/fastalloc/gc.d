61e895dc9e03cb36bd0918a5c6c733207b090891
Per Nordlöw <per.nordlow@gmail.com>
2018-10-07 00:32:29 +0200

    *** empty log message ***

--- src/gc/impl/fastalloc/gc.d
+++ src/gc/impl/fastalloc/gc.d
@@ -280,6 +280,8 @@ struct SmallPools
             }
         default:
             blkinfo.base = null;
+            printf("### %s(size:%lu, bits:%u) Cannot handle blkinfo.size:%lu\n", __FUNCTION__.ptr, size, bits, blkinfo.size);
+            onOutOfMemoryError();
             assert(0, "Handle other blkinfo.size");
         }
 
