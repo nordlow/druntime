0e5a250d2ff23226f8297f1b9646187242f1c6f8
Per Nordlöw <per.nordlow@gmail.com>
2018-10-09 13:44:09 +0200

    *** empty log message ***

--- src/gc/impl/fastalloc/gc.d
+++ src/gc/impl/fastalloc/gc.d
@@ -79,7 +79,8 @@
  *
  * References:
  * 0. Proposal: Dense mark bits and sweep-free allocation
- *    https://github.com/golang/proposal/blob/master/design/12800-sweep-free-alloc.md
+ *    https://github.com/golang/proposal/blob/master/design/12800-sweep-free-alloc.md and in turn
+ *    https://github.com/golang/go/issues/12800
  * 1. Inside D's GC:
  *    https://olshansky.me/gc/runtime/dlang/2017/06/14/inside-d-gc.html
  * 2. DIP 46: Region Based Memory Allocation
